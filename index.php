9c9182dc072104036eb31216ae51d76af6611ff4
root <root@DESKTOP-SAV26J5.localdomain>
2023-08-11 01:02:25 +0530

    Resetting

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if ( current_user_can( 'activate_plugins' ) ) {
-	esc_html_e( 'This theme was built for the WordPress full-site-editing feature. If you use WordPress below version 5.9, you need to install and activate the Gutenberg plugin to make it work. ', 'zeever' );
-}
